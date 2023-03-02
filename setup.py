dabcff733e72c236ae168750c0c8754a215ac4df
lmatosevic <lukamatosevic5@gmail.com>
2023-03-02 20:34:18 +0100

    chore: version 1.0.4

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ Command line interface tool for interacting with ChatGPT using your favorite ter
 """
 
 setup(name="chatgpt-cli-tool",
-      version="1.0.3",
+      version="1.0.4",
       author="luka",
       author_email="lukamatosevic5@gmail.com",
       url='https://github.com/lmatosevic/chatgpt-cli',
-      download_url='https://github.com/lmatosevic/chatgpt-cli/archive/refs/tags/1.0.3.tar.gz',
+      download_url='https://github.com/lmatosevic/chatgpt-cli/archive/refs/tags/1.0.4.tar.gz',
       packages=find_packages(),
       install_requires=["openai", "python-dotenv"],
       python_requires='>=3.7.1',
