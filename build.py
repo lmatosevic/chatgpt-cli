42e6af1ae112b5cd1c03d8d93b412079c4ec0a1e
lmatosevic <lukamatosevic5@gmail.com>
2025-03-11 14:57:42 +0100

    fix: simplified build script

--- build.py
+++ build.py
@@ -1,16 +1,14 @@
 import os
 import shutil
 
+package_name = 'chatgpt_cli_tool'
+
+# Clear and build package files
 shutil.rmtree('dist', ignore_errors=True)
 os.system('python setup.py sdist bdist_wheel')
-shutil.rmtree('chatgpt_cli_tool.egg-info', ignore_errors=True)
+shutil.rmtree(f'{package_name}.egg-info', ignore_errors=True)
 
 # Normalize package name to compy with PEP 625 standard
 version_line = open('./cli/__init__.py', "rt").readline()
 version = version_line.split('=')[-1].strip().replace('\'', '')
-original_filename = f"./dist/chatgpt-cli-tool-{version}.tar.gz"
-name_part, extension = os.path.splitext(original_filename)
-parts = name_part.split('-')
-distribution = '_'.join(parts[:-1])
-new_filename = f"{distribution}-{version}{extension}"
-os.rename(original_filename, new_filename)
+os.rename(f"./dist/{package_name.replace('_', '-')}-{version}.tar.gz", f"./dist/{package_name}-{version}.tar.gz")
