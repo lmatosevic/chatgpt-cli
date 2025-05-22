02e0533273a6d0ab84440edc1ee735d7a52d1c0a
lmatosevic <lukamatosevic5@gmail.com>
2025-05-22 09:00:08 +0200

    fix: use requirements.txt in setup script

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ desc = "Command-line interface tool for interacting with ChatGPT using terminal"
 version_line = open('./cli/__init__.py', "rt").readline()
 version = version_line.split('=')[-1].strip().replace('\'', '')
 
+with open('./requirements.txt', 'r') as file:
+    requirements = [line.strip() for line in file]
+
 setup(name="chatgpt-cli-tool",
       version=version,
       author="luka",
@@ -14,7 +17,7 @@ setup(name="chatgpt-cli-tool",
       url='https://github.com/lmatosevic/chatgpt-cli',
       download_url=f'https://github.com/lmatosevic/chatgpt-cli/archive/refs/tags/{version}.tar.gz',
       packages=find_packages(),
-      install_requires=["openai==0.28.1", "python-dotenv==1.0.0", "colorama==0.4.6", "prompt-toolkit==3.0.39"],
+      install_requires=requirements,
       python_requires='>=3.8.0',
       entry_points={
           'console_scripts': [
